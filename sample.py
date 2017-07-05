0927575db8346baa31fb2d3e019087ee4918db73
= <prabhakar9009@gmail.com>
2017-07-05 10:12:41 +0530

    adding all files

---
+++ sample.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+print "this is my first python script"
+jack = 30
+jac1 = 45
+print "this is the value:" ,jack
+print "this is the valuse:" ,jac1
+var = "hellow world"
+print var
+var1 = "hellow world this is my new project"
+print var1
+print (var [2:])
+print (var1 [2:7])
+print (var+var1)
+var3 = ['abdn','jack','1234','88787','jabalalika']
+print var3 [1]
+print var3 [4]
+var2 = ['abcj','moksk','1812828']
+print var3 + var2
+print var3
+praba = {'name':'prabhakar','roll':'105'}
+print praba['name']
+print praba['roll']
